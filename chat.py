18e76b6937b596cec2a532aecae0ac146d1430e2
CatarinaCosta02 <catarinateves02@gmail.com>
2024-04-28 02:53:16 +0100

    load de dados

--- chat.py
+++ chat.py
@@ -1,22 +1,69 @@
-# LangChain supports many other chat models. Here, we're using Ollama
 from langchain_community.chat_models import ChatOllama
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import ChatPromptTemplate
 
-# supports many more optional parameters. Hover on your `ChatOllama(...)`
-# class to view the latest available supported parameters
-llm = ChatOllama(model="llama3")
-prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
+template = """
+Answer the question based on the context below. If you can't 
+        answer the question, reply "I don't know".
 
-# using LangChain Expressive Language chain syntax
-# learn more about the LCEL on
-# /docs/expression_language/why
-chain = prompt | llm | StrOutputParser()
+        Context: {context}
 
-# for brevity, response is printed in terminal
-# You can use LangServe to deploy your application for
-# production
-print(chain.invoke({"topic": "Space travel"}))
+        Question: {question}
+"""
 
-# pip install llama-cpp-python
-# ollama pull llama3
+def chat_with_ollama():
+    print("Entrei")
+    messages = []
+    # Inicializando o modelo Ollama com o modelo "llama3"
+    llm = ChatOllama(model="llama3")
+    #prompt = ChatPromptTemplate.from_template("Tell me a short joke about {topic}")
+
+    prompt = ChatPromptTemplate.from_template(template)
+    prompt.format(context="I´m bad at physics", question="I'd like to understand string theory")
+    parser = StrOutputParser()
+    chain = prompt | llm | parser
+    response = chain.invoke({
+        "context": "I´m bad at physics",
+        "question": "I'd like to understand string theory"
+    })
+    
+    content = response.strip().lower()
+    # add latest response to the chat
+    messages.append(content)
+    print(f'Champi: {content}\n')
+
+    if "true" in content:
+        # Print response in Green if it is correct.
+        print("\033[92m" + f"Response: {content}" + "\033[0m")
+        return True
+    """
+    # Loop infinito para continuar pedindo perguntas ao usuário
+    while True:
+        # Solicita uma pergunta ao usuário
+        # user_input = input("Digite sua pergunta ou 'exit' para sair: ")
+        user_input = input(f'>> ')
+
+        # Verifica se o usuário digitou 'exit' para sair do loop
+        if user_input.lower() == 'exit':
+            break
+        
+        # Invoca o modelo de chat com a pergunta do usuário
+        # Aqui, estamos passando a mensagem diretamente para o modelo,
+        # sem a necessidade de um dicionário com um tópico.
+        # Como estamos em um loop, podemos simplesmente passar a mensagem do usuário.
+        
+        # Invoca o modelo de chat com a entrada do usuário
+        response = llm.invoke(user_input)
+        
+        # Extrai o conteúdo da resposta
+        content = response.content.strip()
+        
+        # Imprime a resposta do modelo de chat
+        print(f'Champi: {content}\n')
+        
+        # Imprime a resposta do modelo de chat
+        #print(chat_model_response)
+    """
+
+# Chama a função para iniciar a interação
+chat_with_ollama()
--- loader.py
+++ loader.py
@@ -2,48 +2,61 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_community.document_loaders.csv_loader import CSVLoader
 from langchain_text_splitters import MarkdownHeaderTextSplitter
 
-# Load the PDF file
-loader = PyPDFLoader("data/plan/3daybeginners.pdf")
-pages = loader.load_and_split()
-pages[0]
-#print(pages[0])
+PATH = "data/plan/3daybeginners.pdf"
 
-chunk = chunk = pages[0].page_content
+def load_docs():
+    # Load pdfs
+    doc_loader = PyPDFLoader(PATH)
+    pages = doc_loader.load_and_split()
+    print("Pages: ", len(pages))
+    return pages
+
+def split_docs(pages):
+    # Split the pdf data
+    chunk = chunk = pages[0].page_content
+    headers_to_split_on = [
+        ("#", "Header 1"),
+        ("##", "Header 2"),
+        ("###", "Header 3"),
+    ]
+    # MD splits
+    markdown_splitter = MarkdownHeaderTextSplitter(
+        headers_to_split_on=headers_to_split_on, strip_headers=False
+    )
+    md_header_splits = markdown_splitter.split_text(chunk)
+
+    # Char-level splits
+    from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+    chunk_size = 500
+    chunk_overlap = 50
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
+
+    # Split
+    splits = text_splitter.split_documents(md_header_splits)
+    print(splits)
+    return splits
+#----------------TO DO-----------------------
+def csv_loader(PATH):
+    loader = CSVLoader(PATH, encoding='utf-8')
+    data = loader.load()
+
+    print(data)
+
+def pinecome_populate():
+    pass
+def chunks_id():
+    pass
+def clear_db():
+    pass
 
 
-# Split the pdf data
-headers_to_split_on = [
-    ("#", "Header 1"),
-    ("##", "Header 2"),
-    ("###", "Header 3"),
-]
-# MD splits
-markdown_splitter = MarkdownHeaderTextSplitter(
-    headers_to_split_on=headers_to_split_on, strip_headers=False
-)
-md_header_splits = markdown_splitter.split_text(chunk)
+def main():
+    pages = load_docs()
+    splits = split_docs(pages)
+    csv_loader()
 
-# Char-level splits
-from langchain_text_splitters import RecursiveCharacterTextSplitter
-
-chunk_size = 500
-chunk_overlap = 50
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=chunk_size, chunk_overlap=chunk_overlap
-)
-
-# Split
-splits = text_splitter.split_documents(md_header_splits)
-splits
-
-#print(splits)
-print(splits[0])
-# load dos CSVs
-
-#loader = CSVLoader(file_path='data/datasets/df_exer.csv', encoding='utf-8')
-#data = loader.load()
-#print('                ')
-#print('CVSSSSS')
-#print(data)
-
-# função para colocar os dados na base de dados
+if __name__ == "__main__":
+    main()
