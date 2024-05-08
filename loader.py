d8ebad582ca5f1b1bb01007f13c6acf02e719d69
CatarinaCosta02 <catarinateves02@gmail.com>
2024-05-08 21:33:21 +0100

    extract-data

--- loader.py
+++ loader.py
@@ -4,6 +4,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
 from langchain_community.embeddings import OllamaEmbeddings
 import re
+import pandas as pd
 
 
 PATH = "data/plan/3daybeginners.pdf"
@@ -50,27 +51,73 @@ def split_docs(documents: list[Document]):
     return documents
 
 
-def extract_metadata(doc_content): ############################################### CONTINUAR
+import pandas as pd
+
+def extract_metadata(doc_content):
+    doc_content = str(doc_content).lower()
     metadata = {}
-    lines = doc_content.split('\n')
+    dic1 = {}   
+    try:
+        # Lista de dias da semana para procurar
+        days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        weeks = ['week 1', 'week 2', 'week 3', 'week 4', 'week 5', 'week 6', 'week 7', 'week 8', 'week 9', 'week 10', 'week 11', 'week 12']
+        
+        # Itera através de cada linha no conteúdo do documento
+        for line in doc_content.split('\n'):
+            # Verifica se algum dos dias da semana está presente na linha
+            for day in days_of_week:
+                if day in line:
+                    metadata['day'] = day
+                    break  # Pare de procurar após encontrar o dia
+            else:
+                metadata['day'] = 'any'  # Se nenhum dia for encontrado, atribui 'any'
+            
+            # Verifica se alguma das semanas está presente na linha
+            for week in weeks:
+                if week in line:
+                    metadata['week'] = week
+                    break
+            else:
+                metadata['week'] = 'any'  # Se nenhuma semana for encontrada, atribui 'any'
+            
+            # Tenta extrair o nome do exercício
+            if ' - ' in line:
+                metadata['exercise_name'] = line.split(' - ')[1].strip()
+                break  # Sai do loop após encontrar o nome do exercício
+            
+            # Se 'day' e 'week' forem encontrados, sai do loop
+            if 'day' in metadata and 'week' in metadata:
+                break
 
-    # Extract day of the week
-    metadata['day'] = lines[0].split(' - ')[0]
-    metadata['exercise_name'] = lines[0].split(' - ')[1]
+    except Exception as e:
+        print(f"Erro: {e}")
 
-    # Extract notes
-    notes_index = lines.index('Notes')
-    notes = '\n'.join(lines[notes_index + 1:])
-    metadata['notes'] = notes
 
+    df = pd.read_csv('data/datasets/df_exer.csv')
+
+
+    unique_types = df['Type'].unique()
+    unique_title = df['Title'].unique()
+    unique_bodyparts = df['BodyPart'].unique()
+
+    print(unique_types)
+    print(unique_title)
+    print(unique_bodyparts)
+
+    
     return metadata
 
 
-def transform_text(text):    
-    # Transformar o texto
-    text = text.lower()  # Converter para minúsculas
-    text = (re.sub(r'[^\w\s]', '', text)).strip()
-    return text
+
+
+# def transform_text(text):    
+#     # Transformar o texto
+#     text_array = []
+#     for t in text:
+#         t = (str(t)).lower()  # Converter para minúsculas
+#         t = (re.sub(r'[^\w\s]', '', t)).strip()
+#         text_array.append(t)
+#     return text_array
 
 
 def save_docs_to_txt(documents, filename):
