1a928005f5278f68e3c11081d93cbbc5f63c5f81
martaclba <martaclba@hotmail.com>
2024-05-12 23:55:02 +0100

    fix metadata

--- loader.py
+++ loader.py
@@ -7,17 +7,17 @@ import re
 import pandas as pd
 
 
-PATH = "data/plan/3daybeginners.pdf"
+# PATH = "data/plan/3daybeginners.pdf"
 
-def load_docs():
-    doc_loader = PyPDFLoader(PATH)
-    pages = doc_loader.load_and_split()
-    print("Pages: ", len(pages))
-    print(f'*from load_docs type(pages): {type(pages)}')
-    return pages
+# def load_docs():
+#     doc_loader = PyPDFLoader(PATH)
+#     pages = doc_loader.load_and_split()
+#     # print("Pages: ", len(pages))
+#     # print(f'*from load_docs type(pages): {type(pages)}')
+#     return pages
 
 
-# paths = ["data/plan/3daybeginners.pdf", 
+# paths = ["data/plan/3daybeginners.pdf",
 #          "data/plan/3dayworkoutroutineanddietforbeginners.pdf",
 #          "data/plan/8weekbeginnerfatlossworkoutforwomen_0.pdf",
 #          "data/plan/8weekbeginnerworkoutforwomen.pdf",
@@ -25,14 +25,15 @@ def load_docs():
 #          "data/plan/startfromscratch.pdf",
 #          "data/plan/thebest15minutewarmups.pdf"]
 
-# def load_docs(paths):
-#     all_pages = []
-#     for path in paths:
-#         doc_loader = PyPDFLoader(path)
-#         pages = doc_loader.load_and_split()
-#         print(f"Pages from '{path}': {len(pages)}")
-#         all_pages.extend(pages)
-#     return all_pages
+
+def load_docs(paths):
+    all_pages = []
+    for path in paths:
+        doc_loader = PyPDFLoader(path)
+        pages = doc_loader.load_and_split()
+        print(f"Pages from '{path}': {len(pages)}")
+        all_pages.extend(pages)
+    return all_pages
 
 
 
@@ -51,73 +52,213 @@ def split_docs(documents: list[Document]):
     return documents
 
 
-import pandas as pd
 
 def extract_metadata(doc_content):
     doc_content = str(doc_content).lower()
+    doc_content = doc_content.removeprefix("page_content='").strip().rstrip("'")
     metadata = {}
-    dic1 = {}   
+
+    print(f'\n\ndoc_content: \n{doc_content}')
+
+    df = pd.read_csv('data/datasets/df_exer.csv')
+
+    # Create an empty dictionary to store the title, type, and body part information
+    exercise_info = {}
+
+    # Iterate over each row in the DataFrame
+    for index, row in df.iterrows():
+        title = row['Title'].lower()
+        exercise_type = row['Type'].lower()
+        body_part = row['BodyPart'].lower()
+
+        # Add the information to the dictionary
+        exercise_info[title] = (exercise_type, body_part)
+
     try:
         # Lista de dias da semana para procurar
         days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+        day_to_number = {day: i+1 for i, day in enumerate(days_of_week)}
         weeks = ['week 1', 'week 2', 'week 3', 'week 4', 'week 5', 'week 6', 'week 7', 'week 8', 'week 9', 'week 10', 'week 11', 'week 12']
-        
-        # Itera através de cada linha no conteúdo do documento
-        for line in doc_content.split('\n'):
-            # Verifica se algum dos dias da semana está presente na linha
-            for day in days_of_week:
-                if day in line:
-                    metadata['day'] = day
-                    break  # Pare de procurar após encontrar o dia
-            else:
-                metadata['day'] = 'any'  # Se nenhum dia for encontrado, atribui 'any'
-            
-            # Verifica se alguma das semanas está presente na linha
-            for week in weeks:
-                if week in line:
-                    metadata['week'] = week
-                    break
-            else:
-                metadata['week'] = 'any'  # Se nenhuma semana for encontrada, atribui 'any'
-            
-            # Tenta extrair o nome do exercício
-            if ' - ' in line:
-                metadata['exercise_name'] = line.split(' - ')[1].strip()
-                break  # Sai do loop após encontrar o nome do exercício
-            
-            # Se 'day' e 'week' forem encontrados, sai do loop
-            if 'day' in metadata and 'week' in metadata:
-                break
+
+
+        # metadata_text = doc_content.split('\n')
+        # print(f'\n\n\nmetadata["text"] - {metadata_text[0]}\n\n\n')
+
+        # Verifica se algum dos dias da semana está presente na linha
+        days_found = []
+        first_day = True
+        for day in days_of_week:
+            if day in doc_content:
+                days_found.append(day)
+        if days_found:
+            metadata['days'] = ''
+            for day in days_found:
+                if first_day:
+                    first_day = False
+                    metadata['days'] = str(day_to_number[day])
+                else:
+                    metadata['days'] += ', ' + str(day_to_number[day])
+        else:
+            metadata['days'] = 'any'
+
+        # Verifica se alguma das semanas está presente na linha
+        weeks_found = []
+        for week in weeks:
+            if week in doc_content:
+                weeks_found.append(week)
+        if weeks_found:
+            for week in weeks_found:
+                metadata['week'] = week
+        else:
+            metadata['week'] = 'any'
+
+
+        # if ' - ' in line:
+        #     # Extrai a parte do corpo usando o split e remove espaços extras com strip
+        #     metadata['body_part'] = line.split(' - ')[1].strip()
+        #     metadata['exercise_type'] = 'any'
+        #     break
+        # else:
+            # print(line)
+            # Tenta extrair o nome do exercício, tipo e parte do corpo usando expressões regulares
+
+
+        found_exercises = []
+        found_body_parts = []
+
+        for title, (exercise_type, body_part) in exercise_info.items():
+            # Create a regular expression for each part of the exercise information
+            pattern_title = re.compile(r'\b' + title + r'\b')
+            pattern_exercise_type = re.compile(r'\b' + exercise_type + r'\b')
+            pattern_body_part = re.compile(r'\b' + body_part + r'\b')
+
+            # Check if the line contains the title, exercise type, or body part
+            if pattern_title.search(doc_content) or pattern_exercise_type.search(doc_content) or pattern_body_part.search(doc_content):
+                # Found an exercise and body part, add them to the lists
+                found_exercises.append(title)
+                found_body_parts.append(body_part)
+
+        # Convert lists to sets to remove duplicates
+        unique_exercises = set(found_exercises)
+        unique_body_parts = set(found_body_parts)
+
+        # After the loop, check if any exercises or body parts were found
+        if unique_exercises:
+            metadata['exercises'] = list(unique_exercises)  # Convert back to list if needed
+        else:
+            metadata['exercises'] = 'any'
+
+        if unique_body_parts:
+            metadata['body_parts'] = list(unique_body_parts)  # Convert back to list if needed
+        else:
+            metadata['body_parts'] = 'any'
+
+
+        metadata['text'] = f'week: {metadata["week"]}; day: {metadata["days"]}; exercises: {metadata["exercises"]}; body part: {metadata["body_parts"]}'.replace('[', '').replace(']', '').replace('/', '').replace('\\', '')
 
     except Exception as e:
         print(f"Erro: {e}")
 
-
-    df = pd.read_csv('data/datasets/df_exer.csv')
-
-
-    unique_types = df['Type'].unique()
-    unique_title = df['Title'].unique()
-    unique_bodyparts = df['BodyPart'].unique()
-
-    print(unique_types)
-    print(unique_title)
-    print(unique_bodyparts)
-
-    
     return metadata
 
 
 
+# def extract_metadata(doc_content):
+#     doc_content = str(doc_content).lower().removeprefix("page_content='").strip().rstrip("'")
+#     metadata = {}
 
-# def transform_text(text):    
-#     # Transformar o texto
-#     text_array = []
-#     for t in text:
-#         t = (str(t)).lower()  # Converter para minúsculas
-#         t = (re.sub(r'[^\w\s]', '', t)).strip()
-#         text_array.append(t)
-#     return text_array
+#     print(f'\n\ndoc_content: \n{doc_content}')
+
+#     df = pd.read_csv('data/datasets/df_exer.csv')
+
+#     # Create an empty dictionary to store the title, type, and body part information
+#     exercise_info = {}
+
+#     # Iterate over each row in the DataFrame
+#     for index, row in df.iterrows():
+#         title = row['Title'].lower()
+#         exercise_type = row['Type'].lower()
+#         body_part = row['BodyPart'].lower()
+
+#         # Add the information to the dictionary
+#         exercise_info[title] = (exercise_type, body_part)
+
+#     try:
+#         # Extract workout days
+#         days_of_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
+#         day_to_number = {day: i+1 for i, day in enumerate(days_of_week)}
+#         days_found = []
+#         for day in days_of_week:
+#             if day in doc_content:
+#                 days_found.append(day)
+#         if days_found:
+#             for day in days_found:
+#                 metadata['day'] = 'day(s) ' + str(day_to_number[day])
+#         else:
+#             metadata['day'] = 'any'
+
+#         # Extract target muscle groups and exercises
+#         exercises_found = []
+#         for title, (exercise_type, body_part) in exercise_info.items():
+#             pattern = re.compile(r'\b' + title + r'\b')
+#             if pattern.search(doc_content):
+#                 exercises_found.append((title, exercise_type, body_part))
+#         if exercises_found:
+#             metadata['exercises'] = exercises_found
+#         else:
+#             metadata['exercises'] = 'any'
+
+#         # Extract repetitions and sets
+#         pattern_sets_reps = re.compile(r'\d+\s+\d+\s+\d+')
+#         if pattern_sets_reps.search(doc_content):
+#             metadata['sets_reps'] = pattern_sets_reps.findall(doc_content)
+#         else:
+#             metadata['sets_reps'] = 'any'
+
+#         # Extract warmup recommendations
+#         pattern_warmup = re.compile(r'have a\s+\d+\s+min\s+warmup')
+#         if pattern_warmup.search(doc_content):
+#             metadata['warmup'] = pattern_warmup.findall(doc_content)[0]
+#         else:
+#             metadata['warmup'] = 'any'
+
+#         # Extract equipment used
+#         pattern_equipment = re.compile(r'(barbell|bodyweight|cables|dumbbells|machines)')
+#         if pattern_equipment.search(doc_content):
+#             metadata['equipment'] = pattern_equipment.findall(doc_content)
+#         else:
+#             metadata['equipment'] = 'any'
+
+#         # Extract notes on form and technique
+#         pattern_notes = re.compile(r'notes\s+(.*)')
+#         if pattern_notes.search(doc_content):
+#             metadata['notes'] = pattern_notes.findall(doc_content)[0]
+#         else:
+#             metadata['notes'] = 'any'
+
+#         # Extract program details
+#         pattern_program_details = re.compile(r'(main goal|training level|program duration|days per week|time per workout)')
+#         if pattern_program_details.search(doc_content):
+#             metadata['program_details'] = pattern_program_details.findall(doc_content)
+#         else:
+#             metadata['program_details'] = 'any'
+
+#     except Exception as e:
+#         print(f"Error: {e}")
+
+#     return metadata
+
+
+
+
+def transform_text(text):
+    # Transformar o texto
+    text_array = []
+    for t in text:
+        t = (str(t)).lower()  # Converter para minúsculas
+        t = (re.sub(r'[^\w\s]', '', t)).strip()
+        text_array.append(t)
+    return text_array
 
 
 def save_docs_to_txt(documents, filename):
@@ -136,14 +277,13 @@ def clear_db():
 
 
 def main():
-    pages = load_docs()
+    pages = load_docs(paths)
     splits = split_docs(pages)
     for doc in splits:
-        print(f'\ndoc.page_content: {doc}')
+        # print(f'\ndoc.page_content: {doc}')
         metadata = extract_metadata(doc.page_content)
         print(metadata)
-        print()
-    
+
 
 if __name__ == "__main__":
     main()
--- pinecone_test.py
+++ pinecone_test.py
@@ -1,3 +1,4 @@
+import re
 import sys
 import uuid
 from pinecone import ServerlessSpec, Pinecone
@@ -6,10 +7,21 @@ from dotenv import load_dotenv
 import time
 from langchain_community.embeddings import OllamaEmbeddings
 import numpy as np
-from loader import load_docs, split_docs, transform_text
+from loader import load_docs, split_docs, transform_text, extract_metadata
 from langchain_pinecone import PineconeVectorStore
+import json
 
-PATH = "data/plan/3dayworkoutroutineanddietforbeginners.pdf"
+
+
+paths = ["data/plan/3daybeginners.pdf"]
+
+        #  "data/plan/3dayworkoutroutineanddietforbeginners.pdf",
+        #  "data/plan/8weekbeginnerfatlossworkoutforwomen_0.pdf",
+        #  "data/plan/8weekbeginnerworkoutforwomen.pdf",
+        #  "data/plan/12weekfullbodyworkoutroutineforbeginners.pdf",
+        #  "data/plan/startfromscratch.pdf",
+        #  "data/plan/thebest15minutewarmups.pdf"
+
 
 load_dotenv()
 
@@ -39,6 +51,7 @@ if index_name not in existing_indexes:
     while not pc.describe_index(index_name).status['ready']:
         time.sleep(1)
 
+
 for index in pc.list_indexes():
     print(f'index name: {index['name']}')
 
@@ -47,13 +60,16 @@ for index in pc.list_indexes():
 a = index = pc.Index(index_name)
 print(f'index: {a}')
 time.sleep(1)
+
+# index.delete(delete_all=True)
+
 # view index stats
 b = index.describe_index_stats()
 print(f'index stats: {b}')
 
 embeddings = OllamaEmbeddings(model="llama3")
 
-pages = load_docs()
+pages = load_docs(paths)
 documents = split_docs(pages)
 
 
@@ -62,51 +78,77 @@ vector = []
 vector_count=0
 for doc in documents:
     for line in doc.page_content.split('\n'):
-        transformed_line = transform_text(line)
-        print(transformed_line)
-        vector.append(transformed_line)  # Adiciona o transformed_line ao vetor
+
+        line = (str(line)).lower()  # Converter para minúsculas
+        line = (re.sub(r'[^\w\s]', '', line)).strip()
+
+        vector.append(line)  # Adiciona o transformed_line ao vetor
 
         # Verifica se o vetor excede o limite de tamanho
         if len(vector) >= 4096:
             vector_count += 1
-            print(f"Vector size reached or exceeded 4096")
+            print(f"Vector size reached 4096")
             # Adiciona o vetor completo ao dicionário
             vectors_dict[vector_count] = vector
             # Reinicia o vetor para continuar a adição de novos transformed_line
             vector = []
             print(f"Vector has been reset.")
-    
+
     # Adiciona o vetor final ao dicionário, caso ainda exista
     if vector:
         vectors_dict[vector_count] = vector
 
-sys.exit()
 
 print(f'len(vectors_dict.items()): {len(vectors_dict.items())}')
 
 
-embedded_vectors = []
-for vector in vectors_dict.items():
-    vector = vector[1]
-    print(f'\nvector: {vector} \nlen(vector): {len(vector)} \ntype(vector): {type(vector)}')
+# print(f'vectors_dict: {vectors_dict}')
+# print(f'vectors_dict.items(): {vectors_dict.items()}')
 
-    # Embed the vector
-    embedded_vector = embeddings.embed_documents(vector)
-    print(f'\nembedded_vector: {embedded_vector}')
-    embedded_vectors.append(embedded_vector)
+
+# embedded_vectors = []
+# for _, vector in vectors_dict.items():
+#     print(f'Vector: {vector}')
+#     print(f'Length of vector: {len(vector)}')
+
+#     # Embed the vector
+#     embedded_vector = embeddings.embed_documents(vector)
+#     # print(f'Embedded vector: {embedded_vector}')
+#     embedded_vectors.append(embedded_vector)
+# print(f'len(embedded_vectors): {len(embedded_vectors)}')
+
+# # Save the embedded vectors to a JSON file
+# with open('embedded_vectors.json', 'w') as f:
+#     json.dump(embedded_vectors, f)
+# print("Embedded vectors have been saved to 'embedded_vectors.json'.")
+
+
+# Open the JSON file
+with open('embedded_vectors.json', 'r') as f:
+    # Load the JSON data from the file
+    embedded_vectors_data = json.load(f)
+# Now, embedded_vectors_data contains the list of embedded vectors
+# You can assign it to your variable
+embedded_vectors = embedded_vectors_data
+
 
 # Flatten embedded_vectors if it's a list of lists
 flattened_vectors = [item for sublist in embedded_vectors for item in sublist]
-print(f'flattened_vectors: {flattened_vectors}')
+# print(f'Flattened vectors: {flattened_vectors}')
 
-embedded_ids = []
-for vec in range(embedded_vector):
-    id = str(str(uuid.uuid4()))
-    embedded_ids.append(id)
+embedded_ids = [str(uuid.uuid4()) for _ in range(len(flattened_vectors))]
+# print(f'Embedded IDs: {embedded_ids}')
+
+metadata = []
+for doc in vectors_dict.items():
+    mtdt = extract_metadata(doc)
+    metadata.append(mtdt)
+
+print(f'\n\nmetadata: {metadata}')
 
 # Combine vectors into the required structure
-vectors_to_upsert = [{"id": embedded_ids, "values": vector} for vector in flattened_vectors]
-print(f'vectors_to_upsert: {vectors_to_upsert}')
+vectors_to_upsert = [{"id": id, "values": vector, "metadata": metadt} for id, vector, metadt in zip(embedded_ids, flattened_vectors, metadata)]
+# print(f'Vectors to upsert: {vectors_to_upsert}')
 
 # Now, you can use 'vectors_to_upsert' with your index.upsert method
 index.upsert(vectors=vectors_to_upsert)
@@ -118,19 +160,28 @@ print(f'\n------after inserting data------')
 print(index.describe_index_stats())
 
 
-print(f'-------------------TESTAR------------------')
+print(f'\n-------------------TESTAR SIMILAR SEARCH------------------')
 
 #text_field = None
 # initialize the vector store object
 # vectorstore = Pinecone(index, embed_model.embed_query, text_field)
-vectorstore = PineconeVectorStore.from_documents(documents, embeddings, index_name=index_name)
 
-print(f'\nvectorstore: {vectorstore}')
+vectorstore = PineconeVectorStore(index=index, embedding=embeddings, text_key="text")
+# vectorstore = PineconeVectorStore.from_documents(documents, embeddings, index_name=index_name)
+
+# vectorstore = Pinecone(
+#     index_name=index_name,
+#     embedding=embeddings
+# )
+
+print(f'vectorstore: {vectorstore}')
 
 query = "Give me a workout plan"
 vectorstore.similarity_search(query, k=3)
 
+
 sys.exit()
+
 
 chain = (
     {"context": pinecone.as_retriever(), "question": RunnablePassthrough()}
